fa21d407a014de93a203f1949d3570a243d6dee2
parisifabiana <155558861+parisifabiana@users.noreply.github.com>
2024-05-01 20:27:59 +0200

    istruzioni condizionali + operatori logici

---
+++ swat/2-lezione/operatori-logici.php
@@ -0,0 +1,18 @@
+<?php
+    $a = -10;
+    $b= -20;
+
+    // if($a > 0 && $b > 0) {
+    //     echo "i numeri sono entrambi positivi";
+    // } else {
+    //     echo "i numeri non sono entrambi positivi";
+    // }
+
+    if($a > 0 || $b > 0) {
+        echo "almeno uno dei due numeri è positivo";
+    } else {
+        echo "i numeri sono entrambi negativi";
+    }
+
+    echo "\n ciao";
+?>
